c430b68c912daaf98c6dcdbdd08f3cebce2d14b8
MarcoDias299 <bkudra06@gmail.com>
2023-06-23 22:24:57 +0200

    JQ ancor links in navigation done. Need to fix bug with scroll after ancor link animate

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $(function() {
 	let intro_Hight = intro.innerHeight();
 	let scroll_Posion = $(window).scrollTop();
 
+	// Fixed Header
 	$(window).on("scroll load resize", function() {
-		let intro_Hight = intro.innerHeight();
 
 		scroll_Posion = $(this).scrollTop();
 		
@@ -13,7 +13,21 @@ $(function() {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
+		};
 
-		}
+		
+		// Smooth Scroll
+		$("[data-scroll]").on("click", function(event) {
+			event.preventDefault();
+
+			let block_ID = $(this).data("scroll");
+			let block_Offset = $(block_ID).offset().top;
+
+			$("html, body").animate({
+				scrollTop: block_Offset - 75
+			}, 1000);
+		
+		});
+
 	});
 });

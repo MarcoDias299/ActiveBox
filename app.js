aec0a6791b2a3b3420dec01dca8f4e7b6bbd8919
MarcoDias299 <bkudra06@gmail.com>
2023-06-24 17:34:49 +0200

    JQ mobile menu

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ $(function() {
 	let intro = $("#intro");
 	let intro_Hight = intro.innerHeight();
 	let scroll_Posion = $(window).scrollTop();
+	let nav = $("#nav");
+let nav_Toggle =$("#nav_toggle");
 
 	// Fixed Header
 	$(window).on("scroll load resize", function() {
@@ -16,17 +18,26 @@ $(function() {
 		};
 
 	});	
+
 		// Smooth Scroll
 	$("[data-scroll]").on("click", function(event) {
 		event.preventDefault();
 
 		let block_ID = $(this).data("scroll");
 		let block_Offset = $(block_ID).offset().top;
+		nav.toggleClass("show");
 
 	$("html, body").animate({
 		scrollTop: block_Offset - 75
 	}, 550);
 		
 	});
+
+// Burger-Menu
+$(nav_Toggle).on("click", function(event) {
+	event.preventDefault();
+	nav.toggleClass("show");
+
+});
 
 });
